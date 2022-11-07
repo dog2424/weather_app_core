fb1431b4e64ab07d81f2191dab32c28b368d84e2
pietro24 <canevarolopietro@gmail.com>
2022-11-07 10:57:32 +0100

    Update weather_dto.py

--- app/models/weathers/weather_dto.py
+++ app/models/weathers/weather_dto.py
@@ -11,8 +11,28 @@ class WeatherDto():
         self.lon = lon
 
     @classmethod
-    def fromWeater(cls, weather: Weather):
+    def fromWeather(cls, weather: Weather):
         dto = None
         if weather is not None:
             dto = WeatherDto(id=weather.id, description=weather.description,
                              lat=weather.lat, lon=weather.lat, temp=weather.temp)
+
+        return dto
+
+    @classmethod
+    def toWeather(cls, weatherDto) -> Weather:
+
+        weather = Weather()
+
+        if hasattr(weatherDto, 'id'):
+            weather.id = weatherDto.id
+        if hasattr(weatherDto, 'lat'):
+            weather.lat = weatherDto.lat
+        if hasattr(weatherDto, 'lon'):
+            weather.lon = weatherDto.lon
+        if hasattr(weatherDto, 'temp'):
+            weather.temp = weatherDto.temp
+        if hasattr(weatherDto, 'description'):
+            weather.description = weatherDto.description
+
+        return weather
